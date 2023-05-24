0c3e868a874ee43b6c5028129f76c69695ec9391
plavy <tinplavec@gmail.com>
2023-05-24 08:13:39 +0200

    rm not needed

--- src/main/java/com/example/jgp/controller/AdminController.java
+++ src/main/java/com/example/jgp/controller/AdminController.java
@@ -1,13 +1,9 @@
 package com.example.jgp.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.jgp.service.AdminService;
--- src/main/java/com/example/jgp/controller/StanicaController.java
+++ src/main/java/com/example/jgp/controller/StanicaController.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
